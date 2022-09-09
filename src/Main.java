9a1e58d18cf036ee7c714db765634103ad5c1345
safaleks <safaleks@mail.ru>
2022-09-09 22:45:39 +0300

    типы данных

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int costPass = 7862; //стоимость билета
+        int stepForMiles = 20; // шаг стоимости за который дают мили
+
+        System.out.println("Dы получаете " + (costPass / stepForMiles) + " бесплатных миль(и)");
+    }
 }
